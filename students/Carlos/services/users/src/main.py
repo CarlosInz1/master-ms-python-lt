f9c9f860aeb1f36d98793d952b039ea56e28f84a
CarlosInz1 <c2314566@gmail.com>
2025-01-20 12:58:45 -0600

    update 6.5

--- students/Carlos/services/users/src/main.py
+++ students/Carlos/services/users/src/main.py
@@ -5,4 +5,4 @@ app = FastAPI ()
 
 app.include_router(router)
 
-"fSfsfdfd"
+
--- students/Carlos/services/users/src/routes/router.py
+++ students/Carlos/services/users/src/routes/router.py
@@ -7,5 +7,34 @@ router = APIRouter()
 def health():
     return {"status": "ok"}
 
-async def some ():
-    pass
+@router.post("/")
+#usuario creado
+def create_user_route ():
+    return {
+        "message" ,"usuario creado"
+
+    }
+
+@router.put("/{id}")
+def update_user_route (id: int):
+    # usuario actualizado
+    return {
+        "id":id,
+        "message": "usuario autorizado"
+
+    }
+@router.get("/{id}")
+def get_user_route (id: int):
+    # informacion usuario
+    return {
+        "id" : id
+    }
+
+@router.get("/")
+def list_user_route () -> list:
+
+    return [
+        {   "id": 123123   }
+    ]
+
+
